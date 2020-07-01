63e68d22f697979c8d90ee5736dc32d6691e2049
ondrejtrisc <57498031+ondrejtrisc@users.noreply.github.com>
2020-07-01 11:30:52 +0200

    heading

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import React from 'react';
-import './App.css';
+import './App.scss';
 
 function App() {
+
+  
+
   return (
     <div className="App">
+      <header>
+        <h1 class="main-heading">Estate Comparison</h1>
+        <hr class="horizontal-line" />
+        <div class="stripe"></div>
+      </header>
     </div>
   );
 }
